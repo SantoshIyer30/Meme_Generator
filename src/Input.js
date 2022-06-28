4807592edb93bb82092ff09cb30bd9d257d07900
K Santosh Iyer <70837375+SantoshIyer30@users.noreply.github.com>
2022-06-28 17:49:50 +0530

    Add files via upload
    Added API calling with useEffect

--- src/Input.js
+++ src/Input.js
@@ -4,23 +4,72 @@ import React, { useState } from 'react';
 
 
 function Input(){
-    const [meme, setMeme] = useState("");
-    
+    const [meme, setMeme] = useState({
+        image:"https://i.imgflip.com/1ii4oc.jpg",
+        topText: "",
+        bottomText:""
+    });
+
+    const [allMemeImages,setAllMemeImages] = useState([])
+
+    React.useEffect(() => {
+        fetch("https://api.imgflip.com/get_memes")
+            .then(res => res.json())
+            .then(data => setAllMemeImages(data.data.memes))
+    }, [])
+
     function handleClick(){
-        const memesArray = MemesData.data.memes;
-        const randomNumber = Math.floor(Math.random() * memesArray.length)
-        const url = memesArray[randomNumber].url
+        const randomNumber = Math.floor(Math.random() * allMemeImages.length)
+        const url = allMemeImages[randomNumber].url
         console.log(url)
-        setMeme(url)
+        setMeme(prevState => ({
+                ...prevState,
+                image : url
+        }))
+    }
+
+    function handleChange (event){
+        const {name,value} = event.target
+        setMeme(prevMeme => {
+            return{
+                ...prevMeme,
+                [name]: value
+            }
+        })
     }
     return(
         <div className="input" >
-            <div className="type">
-            <input type="text" className="textbox" name="fhalf"/>
-            <input type="text" className="textbox" name="shalf"/>
+            <div className="form">
+
+                <input 
+                type="text" 
+                className="textbox" 
+                name="topText" 
+                value={meme.topText}
+                onChange={handleChange}
+                />
+
+                <input 
+                type="text" 
+                className="textbox" 
+                name="bottomText" 
+                value={meme.bottomText}
+                onChange={handleChange}
+                />
+
+                <button 
+                onClick={handleClick} 
+                className="submit-button" 
+                value="">
+                    Get new meme image 🖼️
+                </button>
+
+            </div> 
+            <div className="meme">
+                <h2 className="meme--text top"> {meme.topText} </h2>
+                <h2 className="meme--text bottom">{meme.bottomText}</h2>
+                <img src = {meme.image} alt ="" className="meme-image" />
             </div>
-            <input type="submit" onClick={handleClick} className="submit-button" value="Get new meme image 🖼️"/>
-            <img src = {meme} alt ="" className="meme-image" />
         </div>
     )
 }
